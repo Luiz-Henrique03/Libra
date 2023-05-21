5dddebcba28d13cb0c1d350c4e032407b5a56c74
Luiz-Henrique03 <cumiranga@gmail.com>
2023-05-20 21:22:27 -0300

    Inferência final

--- inference_classifier.py
+++ inference_classifier.py
@@ -16,7 +16,11 @@ labels_dict = {0:'N',1:'E',2:'Y'}
 
 while True:
     data_aux = []
+    x_ = []
+    y_ = []
     ret, frame = cap.read()
+    H, W, _ = frame.shape()
+
     frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
 
@@ -35,9 +39,21 @@ while True:
 
                 data_aux.append(x)
                 data_aux.append(y)
+                x_.append(x)
+                y_.append(y)
+
+        x1 = int(min(x_) * W)
+        y1 = int(min(y_) * H)
+
+        x2 = int(max(x_) * W)
+        y2 = int(max(y_) * H)
+
         prediction = model.predict([np.asarray(data_aux)])
         predicted_character = labels_dict[int(prediction[0])]
         print(predicted_character)
+        cv2.rectangle(frame,(x1,y1), (x2,y2), (0,0,0), 4)
+        cv2.putText(frame, predicted_character, (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
 
 
 
