72ad321ac70765d3265e11470aff7e86f956f31e
Luiz-Henrique03 <cumiranga@gmail.com>
2023-05-22 21:04:59 -0300

    Update

--- train_classifier.py
+++ train_classifier.py
@@ -1,27 +1,37 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import  accuracy_score
+from sklearn.metrics import accuracy_score
 import numpy as np
 
-data_dict = pickle.load(open('./data.pickle','rb'))
+data_dict = pickle.load(open('./data.pickle', 'rb'))
 
-print(type(data_dict))
+data = []
+labels = []
 
-data = np.asarray(data_dict['data'])
-labels = np.asarray(data_dict['labels'])
+for sample_data, label in zip(data_dict['data'], data_dict['labels']):
+    data.append(sample_data)
+    labels.append(label)
 
-x_train, x_test, y_train, y_test = train_test_split(data,labels,test_size=0.2, shuffle=True,stratify=labels)
+max_data_length = max(len(sample_data) for sample_data in data)
+
+# Preencher amostras com tamanho variável com um valor padrão (0)
+data = [sample_data + [0] * (max_data_length - len(sample_data)) for sample_data in data]
+
+data = np.asarray(data)
+labels = np.asarray(labels)
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 model = RandomForestClassifier()
-
-model.fit(x_train,y_train)
+model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
 
-score = accuracy_score(y_predict,y_test)
+score = accuracy_score(y_predict, y_test)
 
-print('{}% of samples were classified correctly !'.format(score *100))
+print('{}% of samples were classified correctly!'.format(score * 100))
+
 f = open('model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
