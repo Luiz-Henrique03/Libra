084e5c5032f7349f5ab892711dc7d710879d7466
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2023-05-23 21:14:46 -0300

    Update

--- inference_classifier.py
+++ inference_classifier.py
@@ -11,8 +11,8 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-labels_dict ={0:'A', 1:'B', 2:'C', 3:'D', 4:'E'}
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3, max_num_hands=1)
+labels_dict ={0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G'}
 while True:
     data_aux = []
     x_ = []
@@ -22,11 +22,11 @@ while True:
 
     frame_rgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                frame,  # image to draw
+    try:
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
                 hand_landmarks,  # model output
                 mp_hands.HAND_CONNECTIONS,  # hand connections
                 mp_drawing_styles.get_default_hand_landmarks_style(),
@@ -56,7 +56,8 @@ while True:
         cv2.rectangle(frame,(x1,y1), (x2,y2), (0,0,0), 4)
         cv2.putText(frame, predicted_character, (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
-
+    except:
+        print("")
 
 
     cv2.imshow('frame',frame)
--- train_classifier.py
+++ train_classifier.py
@@ -1,26 +1,36 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import  accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
 import numpy as np
 
-data_dict = pickle.load(open('./data.pickle','rb'))
-
+data_dict = pickle.load(open('./data.pickle', 'rb'))
 
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
 
-x_train, x_test, y_train, y_test = train_test_split(data,labels,test_size=0.2, shuffle=True,stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 5, 10],
+    'criterion': ['gini', 'entropy'],
+    'min_samples_split': [2, 5, 10]
+}
 
 model = RandomForestClassifier()
 
-model.fit(x_train,y_train)
+grid_search = GridSearchCV(model, param_grid, scoring='accuracy')
+grid_search.fit(x_train, y_train)
 
-y_predict = model.predict(x_test)
+best_params = grid_search.best_params_
+best_model = grid_search.best_estimator_
 
-score = accuracy_score(y_predict,y_test)
+y_predict = best_model.predict(x_test)
+score = accuracy_score(y_predict, y_test)
 
-print('{}% of samples were classified correctly !'.format(score *100))
+print('{}% of samples were classified correctly!'.format(score * 100))
+
 f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
+pickle.dump({'model': best_model}, f)
 f.close()
